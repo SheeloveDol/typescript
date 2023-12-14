4ed82227a7b19504c7c8b1f45840303b4e0d201d
Sheelove Dol <siesel26@gmail.com>
2023-12-14 00:39:58 -0800

    Examples of typescript objects

---
+++ objects.ts
@@ -0,0 +1,25 @@
+const User = {
+	name: 'Red',
+	email: 'red@mail.com',
+	isActive: true,
+}
+
+const createUser = ({name: string, isPaid: boolean}) => {
+	// Do something here, like return the object
+}
+
+const createSession = (): {} => {
+    return {}
+}
+
+createUser({name: 'Red', isPaid: true})
+
+
+
+
+
+
+
+
+
+export {}
